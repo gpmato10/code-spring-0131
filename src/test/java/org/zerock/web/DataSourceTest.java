57b5a0bb7d5460f07fe934019e8eb8e9ecfe7743
dw <gpmato10@gmail.com>
2016-01-31 21:09:23 +0900

    4.4.1 p89 DataSourceTest.java 까지 완료.

---
+++ src/test/java/org/zerock/web/DataSourceTest.java
@@ -0,0 +1,30 @@
+package org.zerock.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ * Created by dw on 2016. 1. 31..
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:web/WEB-INF/applicationContext.xml")
+public class DataSourceTest {
+    @Inject
+    private DataSource ds;
+
+    @Test
+    public void testConnection() throws Exception {
+        try (Connection con = ds.getConnection()) {
+            System.out.println(con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
